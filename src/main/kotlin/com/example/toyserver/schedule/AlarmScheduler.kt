12b2fbd9b5601fce6276d12f5dbf833bd56fa862
hanjo8813 <hanjo8813@gmail.com>
2023-05-25 00:48:20 +0900

    chore: cicd 설정

--- src/main/kotlin/com/example/toyserver/schedule/AlarmScheduler.kt
+++ src/main/kotlin/com/example/toyserver/schedule/AlarmScheduler.kt
@@ -1,18 +1,25 @@
 package com.example.toyserver.schedule
 
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
-//@Component
+@Component
 class AlarmScheduler(
     @Value("\${slack.webhook.url}") val slackWebhookUrl: String
 ) {
 
     var client: RestTemplate = RestTemplate()
 
-    @Scheduled(fixedDelay = 5 * 1000, zone = "Asia/Seoul")
-    fun leeumSchedule() {
+    @PostConstruct
+    fun temp() {
         println(slackWebhookUrl)
     }
+
+//    @Scheduled(fixedDelay = 60 * 1000, zone = "Asia/Seoul")
+//    fun leeumSchedule() {
+//        println(slackWebhookUrl)
+//    }
 }
